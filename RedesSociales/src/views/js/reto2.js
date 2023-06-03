985621868984ebb9a8e2b171f54f830ba75770a2
MiguelACoronado <125321558+MiguelACoronado@users.noreply.github.com>
2023-06-03 17:31:28 -0500

    JS para evidenciar los eventos mas comunes de JavaScript

---
+++ RedesSociales/src/views/js/reto2.js
@@ -0,0 +1,152 @@
+// -------------------- OnClick ----------------- //
+
+let btn = document.getElementById("btnOnClick")
+
+btn.onclick = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnClick")
+
+
+}
+
+// ---------------- Fin OnClick --------------------//
+
+// ----------------OnBlClick -----------------------//
+
+let btn1 = document.getElementById("btnOnDblClick")
+
+btn1.ondblclick = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnDblClick")
+
+
+}
+
+// ------------------ Fin OnBlClick ---------------//
+
+// ----------------OnKeyDown -----------------------//
+
+let btn2 = document.getElementById("btnOnKeyDown")
+
+btn2.onkeydown = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnKeyDown")
+
+
+}
+
+// ------------------ Fin OnKeyDown ---------------//
+
+// ----------------OnKeyPress -----------------------//
+
+let btn3 = document.getElementById("btnOnKeyPress")
+
+btn3.onkeypress = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnKeyPress")
+
+
+}
+
+// ------------------ Fin OnKeyPress ---------------//
+
+// ----------------OnKeyUp -----------------------//
+
+let btn4 = document.getElementById("btnOnKeyUp")
+
+btn4.onkeyup = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnKeyUp")
+
+
+}
+
+// ------------------ Fin OnKeyUp ---------------//
+
+// ----------------OnMouseDown -----------------------//
+
+let btn5 = document.getElementById("btnOnMouseDown")
+
+btn5.onmousedown = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnMouseDown")
+
+
+}
+
+// ------------------ Fin OnMouseDown ---------------//
+
+// ----------------OnMouseMove -----------------------//
+
+let btn6 = document.getElementById("btnOnMouseMove")
+
+btn6.onmousemove = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnMouseMove")
+
+
+}
+
+// ------------------ Fin OnMouseMove ---------------//
+
+// ----------------OnMouseOver -----------------------//
+
+let btn8 = document.getElementById("btnOnMouseOver")
+
+btn8.onmouseover = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnMouseOver")
+
+
+}
+
+// ------------------ Fin OnMouseOver ---------------//
+
+// ----------------OnMouseUp -----------------------//
+
+let btn9 = document.getElementById("btnOnMouseUp")
+
+btn9.onmouseup = function opciones(e){
+
+    //declarar funcion calcular=(e) =>{}
+    e.preventDefault()
+
+
+    alert("Acabas de usar el evento OnMouseUp")
+
+
+}
+
+// ------------------ Fin OnMouseUp ---------------//
